4002da1db5760a6445bf9f1790df5f28b7240ff3
rivermanvr <vrgithub@riversconsulting.com>
2018-05-29 15:25:03 -0400

    asdded an onClick event for a post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,18 +4,20 @@ import './FullPost.css';
 
 class FullPost extends Component {
     render () {
-        let post = <p>Please select a Post!</p>;
+      let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+      if (this.props.id) {
         post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+          <div className="FullPost">
+              <h1>Title</h1>
+              <p>Content</p>
+              <div className="Edit">
+                  <button className="Delete">Delete</button>
+              </div>
+          </div>
 
         );
-        return post;
+      }
+      return post;
     }
 }
 
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,19 +6,37 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-  state = { posts: [] };
+  state = {
+      posts: [],
+      selectedPostId: null
+    };
 
   componentDidMount () {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        this.setState({ posts: response.data });
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Vince'
+          };
+        });
+        this.setState({ posts: updatedPosts });
         // console.log(response);
     })
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  }
+
   render () {
     const posts = this.state.posts.map(post => {
-      return <Post key={ post.id } title={ post.title } />;
+      return <Post
+        key={ post.id }
+        title={ post.title }
+        author={ post.author }
+        clicked={ () => this.postSelectedHandler(post.id) } />;
     });
     return (
       <div>
@@ -26,7 +44,7 @@ class Blog extends Component {
             { posts }
           </section>
           <section>
-              <FullPost />
+              <FullPost id={ this.state.selectedPostId } />
           </section>
           <section>
               <NewPost />
