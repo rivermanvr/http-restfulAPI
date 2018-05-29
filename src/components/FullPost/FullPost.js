8a1939509bcafb4561857a48f1a715a8176617bd
rivermanvr <vrgithub@riversconsulting.com>
2018-05-29 15:36:18 -0400

    setting up the http request for 1 post that is clicked

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-      let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-      if (this.props.id) {
-        post = (
-          <div className="FullPost">
-              <h1>Title</h1>
-              <p>Content</p>
-              <div className="Edit">
-                  <button className="Delete">Delete</button>
-              </div>
-          </div>
-
-        );
-      }
-      return post;
+  componentDidUpdate () {
+    if (this.props.id) {
+      axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+      .then(response => console.log(response));
     }
+  }
+
+  render () {
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+            <h1>Title</h1>
+            <p>Content</p>
+            <div className="Edit">
+                <button className="Delete">Delete</button>
+            </div>
+        </div>
+
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
