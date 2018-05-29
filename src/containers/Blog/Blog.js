f23f9b638f5daec5007fce1916193e94fdc821f7
rivermanvr <vrgithub@riversconsulting.com>
2018-05-29 13:17:25 -0400

    added an axios call to JSONPlaceholder

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = { posts: [] };
+
   componentDidMount () {
-    axios.get('https://jsonplaceholder.typicode.com/posts');
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.setState({ posts: response.data });
+        // console.log(response);
+    })
   }
 
   render () {
-      return (
-          <div>
-              <section className="Posts">
-                  <Post />
-                  <Post />
-                  <Post />
-              </section>
-              <section>
-                  <FullPost />
-              </section>
-              <section>
-                  <NewPost />
-              </section>
-          </div>
-      );
-  }
+    const posts = this.state.posts.map(post => {
+      return <Post />;
+    });
+    return (
+      <div>
+          <section className="Posts">
+            { posts }
+          </section>
+          <section>
+              <FullPost />
+          </section>
+          <section>
+              <NewPost />
+          </section>
+      </div>
+    );
+}
 }
 
 export default Blog;
