340da6a02218297e352b1be6512a763ee75b6e71
rivermanvr <vrgithub@riversconsulting.com>
2018-05-31 13:55:58 -0400

    setting up "Add Post"

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -9,8 +10,17 @@ class NewPost extends Component {
         author: 'Max'
     }
 
-    
-
+    postDataHandler = () => {
+      const post = {
+        title: this.state.title,
+        body: this.state.body,
+        author: this.state.author
+      };
+      axios.post(`https://jsonplaceholder.typicode.com/posts`, post)
+        .then(response => {
+          console.log(response);
+        })
+    }
     render () {
         return (
             <div className="NewPost">
@@ -24,7 +34,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={ this.postDataHandler }>Add Post</button>
             </div>
         );
     }
