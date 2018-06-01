414ca374639d0fc56b1d936b34f0b3e327921dca
rivermanvr <vrgithub@riversconsulting.com>
2018-06-01 10:13:21 -0400

    created a simple global axios interceptor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+/*  all axios requests pass through this interceptor first
+    You must return the request or you will block requests
+    since this is a "pass through function".  If you want,
+    you can add things to the request.
+*/
+axios.interceptors.request.use(request => {
+  console.log('request: ', request);
+  return request;
+}, error => {
+  console.log('error request: ', error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log('response: ', response);
+  return response;
+}, error => {
+  console.log('error response: ', error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
