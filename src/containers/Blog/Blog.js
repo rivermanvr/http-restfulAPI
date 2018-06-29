d59d4d5bd74ca1129a42fb3dc4aa78900c5d6848
rivermanvr <vrgithub@riversconsulting.com>
2018-06-29 16:53:42 -0400

    rendering posts and full post using components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,6 +8,7 @@ import { Route, NavLink } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
   render () {
@@ -38,6 +39,7 @@ class Blog extends Component {
         </header>
         <Route path="/" exact component={ Posts } />
         <Route path="/new-post" component={ NewPost } />
+        <Route path="/:id" exact component={ FullPost } />
       </div>
     );
   }
