32f248980585241e22494bf84b5d630bbf834ba8
rivermanvr <vrgithub@riversconsulting.com>
2018-07-13 10:35:24 -0400

    showing how to configure a basename within browserrouter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Blog from './containers/Blog/Blog';
 
+// Note:  in deployment, if you have your code sitting
+// within a base folder, you need to tell React this
+// and configure the basename within BrowserRouter
+// The default is "/", but you might have a folder "my-app" as an example
+// then you would need to configure things as follows:
+/* 
+    <BrowserRouter basename="my-app>
+      <div className="App">
+        <Blog />
+      </div>
+    </BrowserRouter> 
+*/
+
+
 class App extends Component {
   render() {
     return (
